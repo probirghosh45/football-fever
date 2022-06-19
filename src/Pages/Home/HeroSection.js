3943fef160a8c979b0d919c5d3eb0c6db0720c49
probir ghosh <probirghosh@outlook.com>
2022-06-19 19:52:42 +0600

    hero section added

---
+++ src/Pages/Home/HeroSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const HeroSection = () => {
+
+    const myStyle={
+        width: '100%',
+        backgroundImage: `url("https://i.ibb.co/Cnb8g2B/fifa-world-cup.jpg")`,
+        }
+
+  return (
+    <div
+      class="hero"
+      style={myStyle}
+    >
+      <div class="hero-content min-h-1 text-center text-neutral-content">
+        <div class="pt-96">
+          <p class="mb-5 text-white pt-20 text-center">The 2022 FIFA World Cup is scheduled to be the 22nd running of the FIFA World Cup competition.It is scheduled to take place in Qatar from 21 November to 18 December 2022</p>
+          <button class="btn btn-warning pt-">Book Your Ticket</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar';
 import Footer from '../Shared/Footer';
+import HeroSection from './HeroSection';
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
+            <HeroSection/>
             <Footer/>
         </div>
     );
