1c4742916924382242c7687874f4e788ec16d328
probir ghosh <probirghosh@outlook.com>
2022-06-22 13:11:28 +0600

    hook issue fixed by using github RaW data

--- src/Components/Groups/GroupAB.js
+++ src/Components/Groups/GroupAB.js
@@ -4,10 +4,10 @@ import GroupCart from '../../Shared/GroupCart';
 
 const GroupAB = () => {
     const [group] = useGroup()
-    // console.log(group);
+    // console.log('AB',group);
 
     const groupAB = group.filter((ab)=>ab.category==="GroupAB")
-    // console.log("AB",groupAB);
+    console.log("AB",groupAB);
 
     return (
         <div class='grid sm:grid-cols-1 justify-items-center md:grid-cols-2 py-8 gap-4'>
--- src/hooks/useGroup.js
+++ src/hooks/useGroup.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useGroup = () => {
   const [group, setGroup] = useState([]);
   useEffect(() => {
-    fetch("./teamData.json")
+    fetch("https://raw.githubusercontent.com/probirghosh45/football-fever/main/public/teamData.json?token=GHSAT0AAAAAABTPCZFSSTXZJEVZAWH7ZW6IYVSYAAA")
       .then((response) => response.json())
       .then((data) => setGroup(data));
   }, []);
