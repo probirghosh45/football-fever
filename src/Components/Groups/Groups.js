3d80025ade2f49ecc67d1085e627680a9bb67fc7
probir ghosh <probirghosh@outlook.com>
2022-06-22 18:53:03 +0600

    active link implimented

--- src/Components/Groups/Groups.js
+++ src/Components/Groups/Groups.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import useGroup from "../../hooks/useGroup";
+import CustomLink from "../../Shared/CustomLink";
 
 const Groups = () => {
 //   const [group] = useGroup();
@@ -8,20 +9,20 @@ const Groups = () => {
   return (
     <div>
       <div className="">
-        <div className="navbar justify-center bg-red-500 py-3">
+        <div className="navbar justify-center bg-green-500 py-3">
           <div className="flex-none ">
             <ul className="menu menu-horizontal p-0 text-xl">
               <li>
-                <Link to={"/"}>Group A & B</Link>
+                <CustomLink to={"/"}>Group A & B</CustomLink>
               </li>
               <li>
-                <Link to={"/group/C-D"}>Group C & D</Link>
+                <CustomLink to={"/group/C-D"}>Group C & D</CustomLink>
               </li>
               <li>
-                <Link to={"/group/E-F"}>Group E & F</Link>
+                <CustomLink to={"/group/E-F"}>Group E & F</CustomLink>
               </li>
               <li>
-                <Link to={"/group/G-H"}>Group G & H</Link>
+                <CustomLink to={"/group/G-H"}>Group G & H</CustomLink>
               </li>
             </ul>
           </div>
